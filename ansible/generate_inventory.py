9238082405616d686da35ce795bb1ace12116a99
Mark Connor <ucabmjc@ucl.ac.uk>
2026-01-22 15:32:40 +0000

    Fix generate_python script

--- ansible/generate_inventory.py
+++ ansible/generate_inventory.py
@@ -1,93 +1,85 @@
 #!/usr/bin/env python3
 
 import json
-import subprocess
 import argparse
+import sys
 
-def run(command):
-    return subprocess.run(command, capture_output=True, encoding='UTF-8')
+IPS_FILE = "ips.json"
+
 
 def generate_inventory():
-    command = "terraform output --json host_ips".split()
-    host_ip_data = json.loads(run(command).stdout)
+    try:
+        with open(IPS_FILE, "r") as f:
+            data = json.load(f)
+    except Exception as e:
+        # Ansible requires valid JSON even on failure
+        return {
+            "_meta": {
+                "hostvars": {}
+            }
+        }
 
-    command = "terraform output --json worker_ips".split()
-    worker_ip_data = json.loads(run(command).stdout)
-    
-    ip_data = host_ip_data + worker_ip_data
+    # Your ips.json format:
+    # [
+    #   {
+    #     "host": "10.x.x.x",
+    #     "workers": ["10.x.x.x", ...]
+    #   }
+    # ]
+    inventory_entry = data[0]
 
-    host_vars = {}
+    host_ip = inventory_entry["host"]
+    worker_ips = inventory_entry["workers"]
 
+    hostvars = {}
 
     # Host group
-    hosts = []
-    for ip in host_ip_data:
-        hosts.append(ip)
-        host_vars[ip] = {"ip": [ip]}
+    hosts = [host_ip]
+    hostvars[host_ip] = {
+        "ip": host_ip
+    }
 
     # Workers group
     workers = []
-    for ip in worker_ip_data:
+    for ip in worker_ips:
         workers.append(ip)
-        host_vars[ip] = {"ip": [ip]}
+        hostvars[ip] = {
+            "ip": ip
+        }
 
-    
-    host_worker_list = [
-        {"host": host_ip, "workers": worker_ip_data}
-        for host_ip in host_ip_data
-    ]
+    inventory = {
+        "_meta": {
+            "hostvars": hostvars
+        },
+        "all": {
+            "children": ["workers"]
+        },
+        "host": {
+            "hosts": hosts
+        },
+        "workers": {
+            "hosts": workers
+        }
+    }
 
-    # Save this mapping to a file
-    with open("ips.json", "w") as f:
-        json.dump(host_worker_list, f, indent=4)
-
-    '''
-    counter = 0
-    workers = []
-
-    for a in ip_data:
-        name = a
-        host_vars[name] = { "ip": [a] }
-        workers.append(name)
-        counter += 1
-    '''
-    _meta = {}
-    _meta["hostvars"] = host_vars
-    _all = { "children": ["workers"] }
-
-    _host = {"hosts": hosts}
-    _workers = { "hosts": workers }
-
-    _jd = {}
-    _jd["_meta"] = _meta
-    _jd["all"] = _all
-    _jd["workers"] = _workers
-    _jd["host"] = _host
+    return inventory
 
 
-    jd = json.dumps(_jd, indent=4)
-    return jd
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--list", action="store_true")
+    parser.add_argument("--host", action="store")
+
+    args = parser.parse_args()
+
+    if args.list:
+        print(json.dumps(generate_inventory(), indent=4))
+    elif args.host:
+        # Required by Ansible, even if unused
+        print(json.dumps({}))
+    else:
+        print(json.dumps({}))
 
 
 if __name__ == "__main__":
-
-    ap = argparse.ArgumentParser(
-        description = "Generate an inventory from Terraform.",
-        prog = __file__
-    )
-
-    mo = ap.add_mutually_exclusive_group()
-    mo.add_argument("--list",action="store", nargs="*", default="dummy", help="Show JSON of all managed hosts")
-    mo.add_argument("--host",action="store", help="Display vars related to the host")
-
-    args = ap.parse_args()
-
-    if args.host:
-        print(json.dumps({}))
-    elif len(args.list) >= 0:
-        jd = generate_inventory()
-        print(jd)
-    else:
-        raise ValueError("Expecting either --host $HOSTNAME or --list")
-
-    
+    main()
